44d67c642db6d5f3075d7932b8d5345ddb66cf2c
Abscentee13 <svihaper@gmail.com>
2024-06-07 01:41:50 +0300

    cors update

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const morgan = require("morgan");
 const { closePool } = require("./config/database");
 
-// Middleware для обробки CORS
+// Middleware для обробки CORS change
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://epic-saga-git-master-epicsagas-projects.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
