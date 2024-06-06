d7a77f3ca2ad6f18271644629079ce4fa42f7ef7
OpamLNL <nataliya.linchuk@uzhnu.edu.ua>
2024-06-07 01:20:33 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const { closePool } = require("./config/database");
 
 // Middleware для обробки CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://epic-saga-git-master-epicsagas-projects.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
