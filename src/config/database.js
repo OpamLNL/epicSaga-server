b00ad44bb5bdaee18542cf50fdfabf167b73db50
Abscentee13 <svihaper@gmail.com>
2024-06-10 23:47:32 +0300

    auth fix

--- src/config/database.js
+++ src/config/database.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     waitForConnections: true,
-    connectionLimit: 10,
+    connectionLimit: 20,
     queueLimit: 0,
     ssl: {
         ca: caCert,
